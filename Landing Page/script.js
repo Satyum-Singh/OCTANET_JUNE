81ac1e37e010ce2eaae1d1f4edeab741857b72c9
Satyum <satyumspathania@gmail.com>
2023-06-06 06:16:36 +0530

    Completed

--- Landing Page/script.js
+++ Landing Page/script.js
@@ -8,17 +8,7 @@ const closeMenu = () => {
   hamburger.classList.remove("active");
 };
 
-// const menu = document.getElementById("menubtn");
-
 function menu() {
   var hamburger = document.getElementById("hamBurger");
   hamburger.classList.add("active");
 }
-
-// document.querySelector("aside buttons.close").onClick = (e) => {
-//   closeMenu();
-// };
-
-// document.querySelector(".backdrop").onClick = (e) => {
-//   closeMenu();
-// };
--- Landing Page/scroll.js
+++ Landing Page/scroll.js
@@ -1,7 +1,23 @@
 document.lastScrollPosition = 0;
+document.lastCentered = 0;
+
 document.addEventListener("scroll", () => {
   const direction =
-    window.apgeYOffset - document.lastScrollPosition > 0 ? "down" : "up";
+    window.pageYOffset - document.lastScrollPosition > 0 ? "down" : "up";
+  const sections = [...document.querySelectorAll("section")];
 
-  const sections = (document.document.lastScrollPosition = window.pageYOffset);
+  const destIndex =
+    direction === "up" ? document.lastCentered - 1 : document.lastCentered + 1;
+
+  if (destIndex >= 0 && destIndex < section.length) {
+    window.scroll(0, sections[destination].offsetTop);
+  }
+
+  sections.forEach((section, index) => {
+    if (window.pageYOffset === section.offsetTop) {
+      document.lastCentered = index;
+    }
+  });
+
+  document.lastScrollPosition = window.pageYOffset;
 });
